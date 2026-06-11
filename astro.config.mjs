ca34441d62a6ac5042c3c8f65fa7d5b329e74c34
David Umoru <umorudavido@gmail.com>
2026-06-11 09:50:59 +0100

    update environment configuration and .gitignore for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel(),
+  env: {
+    schema: {
+      YOUTUBE_API_KEY: envField.string({ context: "server", access: "secret" }),
+      SPOTIFY_CLIENT_ID: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+      SPOTIFY_CLIENT_SECRET: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
+});
